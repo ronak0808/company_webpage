bdf1c08bb140b16edec83922e6ed8eff07786478
rounak <rounakrunwal08@gmail.com>
2025-01-24 15:49:49 +0530

    Fix .env issue2

--- company/settings.py
+++ company/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import certifi
-from decouple import config,RepositoryEnv
+from decouple import Config,RepositoryEnv
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key')
+SECRET_KEY = Config('SECRET_KEY', default='fallback-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -150,6 +150,6 @@ EMAIL_HOST='smtp.googlemail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='rounakrunwal08@gmail.com'
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD=Config('EMAIL_HOST_PASSWORD')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
